c89533cb1d20f424cfaf8742e2ea057b1e6857ae
Madou <ansonhorse@users.noreply.github.com>
2024-01-30 17:54:19 +0800

    Update tk-id-to-url.js

--- tk-id-to-url.js
+++ tk-id-to-url.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         淘宝联盟搜索id时自动生成商品链接
 // @namespace    http://pub.alimama.com/
-// @version      0.2
+// @version      0.3
 // @description  有时候从别处得到一个商品id(只有id，不是链接)，想知道该商品是否在淘宝联盟，或者需要查看该商品的佣金信息等，还得手动写一个链接`https://item.taobao.com/item.htm?id=xxx，才能进行搜索。本脚本在用户输入时，如果检测到关键词是商品id，则自动生成商品链接。
-// @author       Anxon
+// @author       Yaxian
 // @match        http://pub.alimama.com/promo/*
 // @grant        none
 // ==/UserScript==
